bd7e1357708612c467be1e75e826edf03c312029
vinStar <731315455@qq.com>
2013-02-26 10:37:01 +0800

    first commit
    fist commit

---
+++ BlockAndInline/read.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+// read 
+function Test(str){
+	alert(str);	
+	}
+
+$(document).ready(function(){
+	Test("1");
+}) 
+
+$(function(){
+	Test("2");
+	  })
+	  
+jQuery(function($){
+	Test("3");
+	  })
