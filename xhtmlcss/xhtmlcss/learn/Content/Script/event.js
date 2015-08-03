7acd32c6af38b1c4dc7b1e22afae4b30239ae380
vin <731315455@qq.com>
2015-08-03 13:50:59 +0800

    event

---
+++ xhtmlcss/xhtmlcss/learn/Content/Script/event.js
@@ -0,0 +1,63 @@
+﻿var EventUtil = {
+    //为元素添加事件
+    addHandler: function (element, type, handler) {
+        if (element.addEventListener) {
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = handler;
+        }
+    },
+    //移除元素事件
+    removeHandler: function (element, type, handler) {
+        if (element.removeEventListener) {
+            element.removeEventListener(type, handler, false);
+        } else if (element.detachEvent) {
+            element.detachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = null;
+        }
+    },
+    //获取事件对象
+    getEvent: function (event) {
+        return event ? event : window.event;
+    },
+    //获取事件对象元素
+    getTarget: function (event) {
+        return event.target || event.srcElement;
+    },
+    preventDefault: function (event) {
+        if (event.preventDefault) {
+            event.preventDefault();
+        } else {
+            event.returnValue = false;
+        }
+    },
+    //阻止冒泡
+    stopPropagation: function (event) {
+
+        if (event.stopPropagation) {
+            event.stopPropagation();
+        } else {
+            event.cancelBubble = true;
+        }
+    },
+    //鼠标滚轮
+    getWheelDelta: function (event) {
+        if (event.wheelDelta) {
+            return (client.engine.opera && client.engine.opera < 9.5 ?
+            -event.wheelDelta : event.wheelDelta);
+        } else {
+            return -event.detail * 40;
+        }
+    },
+    //字符编码
+    getCharCode: function (event) {
+        if (typeof event.charCode == "number") {
+            return event.charCode;
+        } else {
+            return event.keyCode;
+        }
+    }
+};
